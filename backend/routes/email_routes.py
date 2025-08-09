6cf91c4b657fcfc67ebb1f2d11160286ab581b59
johnwongjething <johnwongjething@gmail.com>
2025-08-09 16:06:12 +0800

    Allow front12311

--- backend/routes/email_routes.py
+++ backend/routes/email_routes.py
@@ -1,6 +1,6 @@
 import os
 import json
-import datetime
+from datetime import datetime
 import time
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
